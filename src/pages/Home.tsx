1d3abee33c1b1d19cc4f3df169a2e81d28135086
Elliottscode <elliottmccully@yahoo.com>
2023-02-08 14:36:33 +0000

    Started making pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,29 @@
 import * as React from "react";
 import { Paper, Typography, Box, Container } from "@mui/material";
 
+import homeStyle from "./home.scss";
+
+
+
 export const Home = () => (
 
-    <div>
-        <h1 id="abh1">Home</h1>
+  <Container >
+    <Box className={homeStyle.box} sx={{ width: '100%'}}>
+        <Typography variant="h1">
+            This is text
+        </Typography>
 
-        
-  
+        <Typography sx={{ align:"left"}}>
+            This is what i am talking about and such is 
+            the way to go on about shit
+            the way to go on about shit
+            the way to go on about shit
+            the way to go on about shit
+            the way to go on about shit
+        </Typography>
+    </Box>
 
-    </div>
+  </Container>
 
 );
   
