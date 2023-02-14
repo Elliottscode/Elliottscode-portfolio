6283d6492eb1ef536e632317958c4788a916c82f
Elliottscode <elliottmccully@yahoo.com>
2023-02-14 13:40:06 +0000

    Update Footer.tsx

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,44 +1,58 @@
 import * as React from "react";
-import { Paper, Typography, Container, ThemeProvider, Box } from "@mui/material";
+import { Paper, Typography, Container, CssBaseline } from "@mui/material";
+
+
+
+
 
 
 function Footer()  {
 
 
 return (
+    <>
+    <CssBaseline />
     <Paper 
         sx={{
             width: '100%',
             height: '10vh',
             bottom: 0,
-            display: 'flex'}} 
+            display: 'flex',
+            alignItems: 'center',
+            position: 'relative'}} 
         component="footer" 
         square variant="outlined"
         
     >
         <Container sx={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            bgcolor: "black"
+            flexGrow: 1,
+            minWidth: '100%',
+            justifyContent: 'center',
+            alignSelf: 'center',
+            textAlign: 'center',
+            position: 'absolute'
             }}>
 
         
-                <Typography sx={{}} variant="caption">
+                <Typography sx={{fontSize: '1.2rem'}} variant="caption">
                     Created By Elliott :)
 
                 </Typography>
-                <Typography sx={{justifyContent:'right'}} variant="caption">
-                    {`${new Date().toLocaleDateString()}`}
-                </Typography>
+               
         </Container>
-
+        <Typography sx={{align: 'right',
+                        paddingRight: '2%',
+                        right: 0,
+                        position: 'absolute',
+                        fontSize: '1.2rem'   }} variant="caption">
+                {`${new Date().toLocaleDateString()}`}
+        </Typography>
+        
 
 
 
     </Paper>
+    </>
 
 )
 
